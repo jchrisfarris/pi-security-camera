0b855461d0fbf1f97701bba22d4af21d54b553e0
Chris Farris <chris.farris@turner.com>
2018-01-01 07:30:00 -0500

    Wrapped some checking around existance of webhook. Added option to disable falsepositive notifications. Perhaps added the ability to ping @here to image hits

--- lambda/publish-slack-notification.py
+++ lambda/publish-slack-notification.py
@@ -16,23 +16,41 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     logger.info("Received event: " + json.dumps(event, sort_keys=True))
+
+    if 'slack-webhook' not in event['config']:
+        logger.info("No slack-webhook defined. Skipping Slack Function")
+        return(event)
+
+    if 'slack-alert-channel' not in event['config']:
+        logger.error("No Slack Channel Defined. Cannot send slack message")
+        return(event)
+
     filename = event['keyframe_image'].split('/')[-1]
     camera = filename.split('-')[0]
     icon = ":camera:"
-    channel = event['config']['slack-falsepositive-channel']
+    ping = "" # Default is no alert
     if event['Alert'] == "true":
         icon = ":camera_with_flash:"
         channel = event['config']['slack-alert-channel']
+        if event['config']['slack-alert-handle']:
+            ping = "{} - ".format(event['config']['slack-alert-handle']) 
+    else:
+        if 'slack-falsepositive-channel' not in event['config']:
+            logger.info("No slack-falsepositive-channel defined. Not sending this message")
+            return(event)
+        channel = event['config']['slack-falsepositive-channel']
     
     movie_url = "https://s3.amazonaws.com/{}/{}".format(event['bucket'], event['movie_key'])
     image_url = "https://s3.amazonaws.com/{}/{}".format(event['bucket'], event['keyframe_image'])
     attachments = [{"image_url": image_url}]
 
+    message = "{}Image from: {} Alert: {}\n{}\n{}".format(ping, datetime.datetime.now(tz.gettz('US/Eastern')), 
+            event['Alert'], event['index_url'], movie_url)
+
     slack_message = {
         'channel': channel,
         'username': camera,
-        'text': "Image from: {} Alert: {}\n{}\n{}".format(datetime.datetime.now(tz.gettz('US/Eastern')), 
-            event['Alert'], event['index_url'], movie_url),
+        'text': message,
         'icon_emoji': icon,
         'attachments': attachments
     }
@@ -46,6 +64,8 @@ def lambda_handler(event, context):
         logger.error("Request failed: %d %s", e.code, e.reason)
     except URLError as e:
         logger.error("Server connection failed: %s", e.reason)
+
+    return(event)
 
   
 ### END OF FUNCTION ###
